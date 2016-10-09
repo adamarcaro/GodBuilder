d615fc74f15f231023b03ee355ff39857f97b322
Adam Arcaro <adamarcaro@gmail.com>
2016-10-08 23:33:38 -0400

    Initial commit

---
+++ app/src/main/java/com/example/adamtrudeauarcaro/godbuilder/FragmentScylla.java
@@ -0,0 +1,24 @@
+package com.example.adamtrudeauarcaro.godbuilder;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Adam Trudeau Arcaro on 2016-10-08.
+ */
+
+public class FragmentScylla extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.god_scylla, container, false);
+        return myView;
+    }
+}
