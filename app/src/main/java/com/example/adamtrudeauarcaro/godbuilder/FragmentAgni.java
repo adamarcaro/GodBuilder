c3a68fd871c1f7a630e71fc426a1503472cb8d3c
Adam Arcaro <adamarcaro@gmail.com>
2017-03-11 00:12:14 -0500

    SharedPreferences + Agni
    Allows builds to be saved when app is closed. Agni complete (first
    magical God as template).

--- app/src/main/java/com/example/adamtrudeauarcaro/godbuilder/FragmentAgni.java
+++ app/src/main/java/com/example/adamtrudeauarcaro/godbuilder/FragmentAgni.java
@@ -1,11 +1,13 @@
 package com.example.adamtrudeauarcaro.godbuilder;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * Created by Adam Trudeau Arcaro on 2016-10-08.
@@ -15,10 +17,173 @@ public class FragmentAgni extends Fragment {
 
     View myView;
 
-    @Nullable
-    @Override
+    public FragmentAgni() {
+    }
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.god_agni, container, false);
+
+        //Initializing ImageButtons
+        final ImageButton starter = (ImageButton) myView.findViewById(R.id.imageStarterItem);
+        final ImageButton relic1 = (ImageButton) myView.findViewById(R.id.imageRelic1);
+        final ImageButton relic2 = (ImageButton) myView.findViewById(R.id.imageRelic2);
+        final ImageButton item1 = (ImageButton) myView.findViewById(R.id.imageItem1);
+        final ImageButton item2 = (ImageButton) myView.findViewById(R.id.imageItem2);
+        final ImageButton item3 = (ImageButton) myView.findViewById(R.id.imageItem3);
+        final ImageButton item4 = (ImageButton) myView.findViewById(R.id.imageItem4);
+        final ImageButton item5 = (ImageButton) myView.findViewById(R.id.imageItem5);
+        final ImageButton item6 = (ImageButton) myView.findViewById(R.id.imageItem6);
+
+        //Loading build
+        starter.setImageResource(getImage("agni_starter"));
+        relic1.setImageResource(getImage("agni_relic1"));
+        relic2.setImageResource(getImage("agni_relic2"));
+        item1.setImageResource(getImage("agni_item1"));
+        item2.setImageResource(getImage("agni_item2"));
+        item3.setImageResource(getImage("agni_item3"));
+        item4.setImageResource(getImage("agni_item4"));
+        item5.setImageResource(getImage("agni_item5"));
+        item6.setImageResource(getImage("agni_item6"));
+
+        starter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                ItemMagStarterFragment dialog = ItemMagStarterFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemMagStarterFragment.OnListItemClickedListener() {
+                    public void onListItemClick(int resourceId) {
+                        saveImage("agni_starter", resourceId);
+                        starter.setImageResource(resourceId);
+                    }
+                });
+                dialog.show(getFragmentManager(), "starterDialog");
+            }
+        });
+
+        relic1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemRelicFragment dialog = ItemRelicFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemRelicFragment.OnListItemClickedListener() {
+                    public void onListItemClick(int resourceId) {
+                        saveImage("agni_relic1", resourceId);
+                        relic1.setImageResource(resourceId);
+                    }
+                });
+                dialog.show(getFragmentManager(), "relic1Dialog");
+            }
+        });
+
+        relic2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemRelicFragment dialog = ItemRelicFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemRelicFragment.OnListItemClickedListener() {
+                    public void onListItemClick(int resourceId) {
+                        saveImage("agni_relic2", resourceId);
+                        relic2.setImageResource(resourceId);
+                    }
+                });
+                dialog.show(getFragmentManager(), "relic2Dialog");
+            }
+        });
+
+        item1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemMagFragment dialog = ItemMagFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemMagFragment.OnListItemClickedListener() {
+                    public void onListItemClick(int resourceId) {
+                        saveImage("agni_item1", resourceId);
+                        item1.setImageResource(resourceId);
+                    }
+                });
+                dialog.show(getFragmentManager(), "item1Dialog");
+            }
+        });
+
+        item2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemMagFragment dialog = ItemMagFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemMagFragment.OnListItemClickedListener() {
+                    public void onListItemClick(int resourceId) {
+                        saveImage("agni_item2", resourceId);
+                        item2.setImageResource(resourceId);
+                    }
+                });
+                dialog.show(getFragmentManager(), "item2Dialog");
+            }
+        });
+
+        item3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemMagFragment dialog = ItemMagFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemMagFragment.OnListItemClickedListener() {
+                    public void onListItemClick(int resourceId) {
+                        saveImage("agni_item3", resourceId);
+                        item3.setImageResource(resourceId);
+                    }
+                });
+                dialog.show(getFragmentManager(), "item3Dialog");
+            }
+        });
+
+        item4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemMagFragment dialog = ItemMagFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemMagFragment.OnListItemClickedListener() {
+                    public void onListItemClick(int resourceId) {
+                        saveImage("agni_item4", resourceId);
+                        item4.setImageResource(resourceId);
+                    }
+                });
+                dialog.show(getFragmentManager(), "item4Dialog");
+            }
+        });
+
+        item5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemMagFragment dialog = ItemMagFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemMagFragment.OnListItemClickedListener() {
+                    public void onListItemClick(int resourceId) {
+                        saveImage("agni_item5", resourceId);
+                        item5.setImageResource(resourceId);
+                    }
+                });
+                dialog.show(getFragmentManager(), "item5Dialog");
+            }
+        });
+
+        item6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemMagFragment dialog = ItemMagFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemMagFragment.OnListItemClickedListener() {
+                    public void onListItemClick(int resourceId) {
+                        saveImage("agni_item6", resourceId);
+                        item6.setImageResource(resourceId);
+                    }
+                });
+                dialog.show(getFragmentManager(), "item6Dialog");
+            }
+        });
+
         return myView;
     }
+
+    public int getImage(String item){
+        SharedPreferences pref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        int id = pref.getInt(item, R.drawable.no_item);
+        return id;
+    }
+
+    public void saveImage(String item, int resourceId){
+        SharedPreferences starterSP = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = starterSP.edit();
+        edit.putInt(item, resourceId);
+        edit.apply();
+    }
+
 }
--- app/src/main/java/com/example/adamtrudeauarcaro/godbuilder/ItemPhysFragment.java
+++ app/src/main/java/com/example/adamtrudeauarcaro/godbuilder/ItemPhysFragment.java
@@ -6,11 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.SearchView.OnQueryTextListener;
+
+import java.util.ArrayList;
 
 
 public class ItemPhysFragment extends DialogFragment {
@@ -18,42 +21,89 @@ public class ItemPhysFragment extends DialogFragment {
     Button btn;
     ListView lv;
     SearchView sv;
-    ArrayAdapter<String> adapter;
-    String[] items={"Breastplate of Valor","Bulwark of Hope","Titan's Bane","Brawler's Beatstick"};
+    String[] names={"Ninja Tabi", "Reinforced Greaves", "Talaria Boots", "Warrior Tabi",
+            "Ancile", "Asi",
+            "Blackthorn Hammer", "Bloodforge", "Brawler's Beat Stick", "Breastplate of Valor", "Bulwark of Hope",
+            "Deathbringer", "Devourer's Gauntlet",
+            "Emperor's Armor",
+            "Frostbound Hammer",
+            "Gauntlet of Thebes", "Genji's Guard", "Gladiator's Shield",
+            "Hastened Fatalis", "Heartseeker", "Heartward Amulet", "Hide of the Nemean Lion", "Hide of the Urchin", "Hydra's Lament",
+            "Ichaival",
+            "Jotunn's Wrath",
+            "Magi's Blessing", "Mail of Renewal", "Malice", "Mantle of Discord", "Masamune", "Midgardian Mail", "Mystical Mail",
+            "Odysseus' Bow", "Oni Hunter's Garb",
+            "Pestilence",
+            "Qin's Sais",
+            "Rage", "Relic Dagger", "Runeforged Hammer", "Runic Shield",
+            "Shield of Regrowth", "Shifter's Shield", "Shogun's Kusari", "Silverbranch Bow", "Soul Eater", "Sovereignty", "Spectral Armor", "Spirit Robe", "Stone Cutting Sword", "Stone of Gaia",
+            "The Crusher", "The Executioner", "Titan's Bane", "Transcendence",
+            "Void Shield",
+            "Wind Demon", "Winged Blade", "Witchblade"};
+    int[] images={R.drawable.ninja_tabi, R.drawable.reinforced_greaves, R.drawable.talaria_boots, R.drawable.warrior_tabi,
+            R.drawable.ancile, R.drawable.asi,
+            R.drawable.blackthorn_hammer, R.drawable.bloodforge, R.drawable.brawlers_beat_stick, R.drawable.breastplate_of_valor, R.drawable.bulwark_of_hope,
+            R.drawable.deathbringer, R.drawable.devourers_gauntlet,
+            R.drawable.emperors_armor,
+            R.drawable.frostbound_hammer,
+            R.drawable.gauntlet_of_thebes, R.drawable.genjis_guard, R.drawable.gladiators_shield,
+            R.drawable.hastened_fatalis, R.drawable.heartseeker, R.drawable.heartward_amulet, R.drawable.hide_of_the_nemean_lion, R.drawable.hide_of_the_urchin, R.drawable.hydras_lament,
+            R.drawable.ichaival,
+            R.drawable.jotunns_wrath,
+            R.drawable.magis_blessing, R.drawable.mail_of_renewal, R.drawable.malice, R.drawable.mantle_of_discord, R.drawable.masamune, R.drawable.midgardian_mail, R.drawable.mystical_mail,
+            R.drawable.odysseus_bow, R.drawable.oni_hunters_garb,
+            R.drawable.pestilence,
+            R.drawable.qins_sais,
+            R.drawable.rage, R.drawable.relic_dagger, R.drawable.runeforged_hammer, R.drawable.runic_shield,
+            R.drawable.shield_of_regrowth, R.drawable.shifters_shield, R.drawable.shoguns_kusari, R.drawable.silverbranch_bow, R.drawable.soul_eater, R.drawable.sovereignty, R.drawable.spectral_armor, R.drawable.spirit_robe, R.drawable.stone_cutting_sword, R.drawable.stone_of_gaia,
+            R.drawable.the_crusher, R.drawable.the_executioner, R.drawable.titans_bane, R.drawable.transcendence,
+            R.drawable.void_shield,
+            R.drawable.wind_demon, R.drawable.winged_blade, R.drawable.witchblade};
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        //Indicate layout for dialog
         View myView=inflater.inflate(R.layout.item_list, null);
-        //SET TITLE DIALOG TITLE
+
+        //Set dialog title
         getDialog().setTitle("Physical Items");
-        //BUTTON,LISTVIEW,SEARCHVIEW INITIALIZATIONS
-        lv=(ListView) myView.findViewById(R.id.listView1);
-        sv=(SearchView) myView.findViewById(R.id.searchView1);
-        btn=(Button) myView.findViewById(R.id.dismiss);
-        //CREATE AND SET ADAPTER TO LISTVIEW
-        adapter=new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,items);
+
+        //Defining searchview, listview, button
+        SearchView sv=(SearchView) myView.findViewById(R.id.searchView1);
+        ListView lv=(ListView) myView.findViewById(R.id.listView1);
+        Button btn=(Button) myView.findViewById(R.id.dismiss);
+
+        //Initialize and set adapter for items
+        final Adapter adapter = new Adapter(getActivity(), this.getItems());
         lv.setAdapter(adapter);
-        //SEARCH
-        sv.setQueryHint("Search..");
+
+        //Searchview functionality
+        //**BUG** Upon searching for an item and selecting it, aegis is returned because index becomes 1
+        sv.setQueryHint("Search items..");
         sv.setOnQueryTextListener(new OnQueryTextListener() {
-            @Override
             public boolean onQueryTextSubmit(String txt) {
-                // TODO Auto-generated method stub
                 return false;
             }
-            @Override
             public boolean onQueryTextChange(String txt) {
-                // TODO Auto-generated method stub
                 adapter.getFilter().filter(txt);
                 return false;
             }
         });
-        //BUTTON
+
+        //Listener for itemclick on list
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            public void onItemClick(AdapterView<?> arg0, View arg1,int position, long arg3)
+            {
+                int image_id = images[position];
+                mListener.onListItemClick(image_id);
+                dismiss();
+            }
+        });
+
+        //Cancel button
         btn.setOnClickListener(new OnClickListener() {
-            @Override
             public void onClick(View arg0) {
-                // TODO Auto-generated method stub
                 dismiss();
             }
         });
@@ -61,9 +111,36 @@ public class ItemPhysFragment extends DialogFragment {
         return myView;
     }
 
-    public static ItemPhysFragment newInstance(){
+    //Fragment contstructor
+    public static ItemPhysFragment newInstance()
+    {
         ItemPhysFragment f = new ItemPhysFragment();
         return f;
+    }
+
+    //Populates list with items
+    private ArrayList<Item> getItems()
+    {
+        ArrayList<Item> items = new ArrayList<Item>();
+        Item item;
+
+        for(int i = 0; i < names.length; i++) {
+            item = new Item(names[i], images[i]);
+            items.add(item);
+        }
+
+        return items;
+    }
+
+    //Setting listener to connect list to God fragment
+    public interface OnListItemClickedListener {
+        void onListItemClick(int resourceId);
+    }
+
+    OnListItemClickedListener mListener;
+
+    public void setOnListItemSelectedListener(OnListItemClickedListener listener) {
+        this.mListener = listener;
     }
 
 }
