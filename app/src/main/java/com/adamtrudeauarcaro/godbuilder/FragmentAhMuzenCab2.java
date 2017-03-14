7862c9adc8a5fdc450859ca4ec9faa112016e2b1
Adam Arcaro <adamarcaro@gmail.com>
2017-03-14 12:07:30 -0400

    UI Overhaul + Multiple Builds (3)
    Completed Agni and AMC

---
+++ app/src/main/java/com/adamtrudeauarcaro/godbuilder/FragmentAhMuzenCab2.java
@@ -0,0 +1,243 @@
+package com.adamtrudeauarcaro.godbuilder;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * Created by Adam Trudeau Arcaro on 2016-10-08.
+ */
+
+public class FragmentAhMuzenCab2 extends Fragment {
+
+    View myView;
+
+    public FragmentAhMuzenCab2() {
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.god_build, container, false);
+
+        //Initializing ImageButtons
+        final ImageButton starter = (ImageButton) myView.findViewById(R.id.imageStarterItem);
+        final ImageButton relic1 = (ImageButton) myView.findViewById(R.id.imageRelic1);
+        final ImageButton relic2 = (ImageButton) myView.findViewById(R.id.imageRelic2);
+        final ImageButton item1 = (ImageButton) myView.findViewById(R.id.imageItem1);
+        final ImageButton item2 = (ImageButton) myView.findViewById(R.id.imageItem2);
+        final ImageButton item3 = (ImageButton) myView.findViewById(R.id.imageItem3);
+        final ImageButton item4 = (ImageButton) myView.findViewById(R.id.imageItem4);
+        final ImageButton item5 = (ImageButton) myView.findViewById(R.id.imageItem5);
+        final ImageButton item6 = (ImageButton) myView.findViewById(R.id.imageItem6);
+
+        //Initializing TextViews
+        final TextView starterName = (TextView) myView.findViewById(R.id.nameStarter);
+        final TextView relic1Name = (TextView) myView.findViewById(R.id.nameRelic1);
+        final TextView relic2Name = (TextView) myView.findViewById(R.id.nameRelic2);
+        final TextView item1Name = (TextView) myView.findViewById(R.id.nameItem1);
+        final TextView item2Name = (TextView) myView.findViewById(R.id.nameItem2);
+        final TextView item3Name = (TextView) myView.findViewById(R.id.nameItem3);
+        final TextView item4Name = (TextView) myView.findViewById(R.id.nameItem4);
+        final TextView item5Name = (TextView) myView.findViewById(R.id.nameItem5);
+        final TextView item6Name = (TextView) myView.findViewById(R.id.nameItem6);
+
+        //Loading build images
+        starter.setImageResource(getImage("ah_muzen_cab_starter_2"));
+        relic1.setImageResource(getImage("ah_muzen_cab_relic1_2"));
+        relic2.setImageResource(getImage("ah_muzen_cab_relic2_2"));
+        item1.setImageResource(getImage("ah_muzen_cab_item1_2"));
+        item2.setImageResource(getImage("ah_muzen_cab_item2_2"));
+        item3.setImageResource(getImage("ah_muzen_cab_item3_2"));
+        item4.setImageResource(getImage("ah_muzen_cab_item4_2"));
+        item5.setImageResource(getImage("ah_muzen_cab_item5_2"));
+        item6.setImageResource(getImage("ah_muzen_cab_item6_2"));
+
+        //Loading build names
+        starterName.setText(getName("ah_muzen_cab_starter_name_2"));
+        relic1Name.setText(getName("ah_muzen_cab_relic1_name_2"));
+        relic2Name.setText(getName("ah_muzen_cab_relic2_name_2"));
+        item1Name.setText(getName("ah_muzen_cab_item1_name_2"));
+        item2Name.setText(getName("ah_muzen_cab_item2_name_2"));
+        item3Name.setText(getName("ah_muzen_cab_item3_name_2"));
+        item4Name.setText(getName("ah_muzen_cab_item4_name_2"));
+        item5Name.setText(getName("ah_muzen_cab_item5_name_2"));
+        item6Name.setText(getName("ah_muzen_cab_item6_name_2"));
+
+
+        starter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                ItemPhysStarterFragment dialog = ItemPhysStarterFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemPhysStarterFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("ah_muzen_cab_starter_name_2", name);
+                        saveImage("ah_muzen_cab_starter_2", resourceId);
+                        starterName.setText(name);
+                        starter.setImageResource(resourceId);
+                    }
+                });
+                dialog.show(getActivity().getFragmentManager(), "starterDialog");
+            }
+        });
+
+        relic1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                ItemRelicFragment dialog = ItemRelicFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemRelicFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("ah_muzen_cab_relic1_name_2", name);
+                        saveImage("ah_muzen_cab_relic1_2", resourceId);
+                        relic1Name.setText(name);
+                        relic1.setImageResource(resourceId);
+                    }
+                });
+                dialog.show(getActivity().getFragmentManager(), "relic1Dialog");
+            }
+        });
+
+        relic2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemRelicFragment dialog = ItemRelicFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemRelicFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("ah_muzen_cab_relic2_name_2", name);
+                        saveImage("ah_muzen_cab_relic2_2", resourceId);
+                        relic2.setImageResource(resourceId);
+                        relic2Name.setText(name);
+                    }
+                });
+                dialog.show(getActivity().getFragmentManager(), "relic2Dialog");
+            }
+        });
+
+        item1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemPhysFragment dialog = ItemPhysFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemPhysFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("ah_muzen_cab_item1_name_2", name);
+                        saveImage("ah_muzen_cab_item1_2", resourceId);
+                        item1.setImageResource(resourceId);
+                        item1Name.setText(name);
+                    }
+                });
+                dialog.show(getActivity().getFragmentManager(), "item1Dialog");
+            }
+        });
+
+        item2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemPhysFragment dialog = ItemPhysFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemPhysFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("ah_muzen_cab_item2_name_2", name);
+                        saveImage("ah_muzen_cab_item2_2", resourceId);
+                        item2.setImageResource(resourceId);
+                        item2Name.setText(name);
+                    }
+                });
+                dialog.show(getActivity().getFragmentManager(), "item2Dialog");
+            }
+        });
+
+        item3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemPhysFragment dialog = ItemPhysFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemPhysFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("ah_muzen_cab_item3_name_2", name);
+                        saveImage("ah_muzen_cab_item3_2", resourceId);
+                        item3.setImageResource(resourceId);
+                        item3Name.setText(name);
+                    }
+                });
+                dialog.show(getActivity().getFragmentManager(), "item3Dialog");
+            }
+        });
+
+        item4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemPhysFragment dialog = ItemPhysFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemPhysFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("ah_muzen_cab_item4_name_2", name);
+                        saveImage("ah_muzen_cab_item4_2", resourceId);
+                        item4.setImageResource(resourceId);
+                        item4Name.setText(name);
+                    }
+                });
+                dialog.show(getActivity().getFragmentManager(), "item4Dialog");
+            }
+        });
+
+        item5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemPhysFragment dialog = ItemPhysFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemPhysFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("ah_muzen_cab_item5_name_2", name);
+                        saveImage("ah_muzen_cab_item5_2", resourceId);
+                        item5.setImageResource(resourceId);
+                        item5Name.setText(name);
+                    }
+                });
+                dialog.show(getActivity().getFragmentManager(), "item5Dialog");
+            }
+        });
+
+        item6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemPhysFragment dialog = ItemPhysFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemPhysFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("ah_muzen_cab_item6_name_2", name);
+                        saveImage("ah_muzen_cab_item6_2", resourceId);
+                        item6.setImageResource(resourceId);
+                        item6Name.setText(name);
+                    }
+                });
+                dialog.show(getActivity().getFragmentManager(), "item6Dialog");
+            }
+        });
+
+        return myView;
+    }
+
+    public int getImage(String item){
+        SharedPreferences pref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        int id = pref.getInt(item, R.drawable.no_item);
+        return id;
+    }
+
+    public void saveImage(String item, int resourceId){
+        SharedPreferences starterSP = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = starterSP.edit();
+        edit.putInt(item, resourceId);
+        edit.apply();
+    }
+
+    public String getName(String item){
+        SharedPreferences pref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        String name = pref.getString(item, "");
+        return name;
+    }
+
+    public void saveName(String item, String name){
+        SharedPreferences starterSP = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = starterSP.edit();
+        edit.putString(item, name);
+        edit.apply();
+    }
+
+}
---
+++ app/src/main/java/com/adamtrudeauarcaro/godbuilder/FragmentAnhur.java
@@ -0,0 +1,246 @@
+package com.adamtrudeauarcaro.godbuilder;
+
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * Created by Adam Trudeau Arcaro on 2016-10-08.
+ */
+
+public class FragmentAnhur extends Fragment {
+
+    View myView;
+
+    public FragmentAnhur() {
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.god_anhur, container, false);
+
+        //Initializing ImageButtons
+        final ImageButton starter = (ImageButton) myView.findViewById(R.id.imageStarterItem);
+        final ImageButton relic1 = (ImageButton) myView.findViewById(R.id.imageRelic1);
+        final ImageButton relic2 = (ImageButton) myView.findViewById(R.id.imageRelic2);
+        final ImageButton item1 = (ImageButton) myView.findViewById(R.id.imageItem1);
+        final ImageButton item2 = (ImageButton) myView.findViewById(R.id.imageItem2);
+        final ImageButton item3 = (ImageButton) myView.findViewById(R.id.imageItem3);
+        final ImageButton item4 = (ImageButton) myView.findViewById(R.id.imageItem4);
+        final ImageButton item5 = (ImageButton) myView.findViewById(R.id.imageItem5);
+        final ImageButton item6 = (ImageButton) myView.findViewById(R.id.imageItem6);
+
+        //Initializing TextViews
+        final TextView starterName = (TextView) myView.findViewById(R.id.nameStarter);
+        final TextView relic1Name = (TextView) myView.findViewById(R.id.nameRelic1);
+        final TextView relic2Name = (TextView) myView.findViewById(R.id.nameRelic2);
+        final TextView item1Name = (TextView) myView.findViewById(R.id.nameItem1);
+        final TextView item2Name = (TextView) myView.findViewById(R.id.nameItem2);
+        final TextView item3Name = (TextView) myView.findViewById(R.id.nameItem3);
+        final TextView item4Name = (TextView) myView.findViewById(R.id.nameItem4);
+        final TextView item5Name = (TextView) myView.findViewById(R.id.nameItem5);
+        final TextView item6Name = (TextView) myView.findViewById(R.id.nameItem6);
+
+        //Loading build images
+        starter.setImageResource(getImage("anhur_starter"));
+        relic1.setImageResource(getImage("anhur_relic1"));
+        relic2.setImageResource(getImage("anhur_relic2"));
+        item1.setImageResource(getImage("anhur_item1"));
+        item2.setImageResource(getImage("anhur_item2"));
+        item3.setImageResource(getImage("anhur_item3"));
+        item4.setImageResource(getImage("anhur_item4"));
+        item5.setImageResource(getImage("anhur_item5"));
+        item6.setImageResource(getImage("anhur_item6"));
+
+        //Loading build names
+        starterName.setText(getName("anhur_starter_name"));
+        relic1Name.setText(getName("anhur_relic1_name"));
+        relic2Name.setText(getName("anhur_relic2_name"));
+        item1Name.setText(getName("anhur_item1_name"));
+        item2Name.setText(getName("anhur_item2_name"));
+        item3Name.setText(getName("anhur_item3_name"));
+        item4Name.setText(getName("anhur_item4_name"));
+        item5Name.setText(getName("anhur_item5_name"));
+        item6Name.setText(getName("anhur_item6_name"));
+
+
+        starter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                ItemPhysStarterFragment dialog = ItemPhysStarterFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemPhysStarterFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("anhur_starter_name", name);
+                        saveImage("anhur_starter", resourceId);
+                        starterName.setText(name);
+                        starter.setImageResource(resourceId);
+                    }
+                });
+                dialog.show(getFragmentManager(), "starterDialog");
+            }
+        });
+
+        relic1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                ItemRelicFragment dialog = ItemRelicFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemRelicFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("anhur_relic1_name", name);
+                        saveImage("anhur_relic1", resourceId);
+                        relic1Name.setText(name);
+                        relic1.setImageResource(resourceId);
+                    }
+                });
+                dialog.show(getFragmentManager(), "relic1Dialog");
+            }
+        });
+
+        relic2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemRelicFragment dialog = ItemRelicFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemRelicFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("anhur_relic2_name", name);
+                        saveImage("anhur_relic2", resourceId);
+                        relic2.setImageResource(resourceId);
+                        relic2Name.setText(name);
+                    }
+                });
+                dialog.show(getFragmentManager(), "relic2Dialog");
+            }
+        });
+
+        item1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemPhysFragment dialog = ItemPhysFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemPhysFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("anhur_item1_name", name);
+                        saveImage("anhur_item1", resourceId);
+                        item1.setImageResource(resourceId);
+                        item1Name.setText(name);
+                    }
+                });
+                dialog.show(getFragmentManager(), "item1Dialog");
+            }
+        });
+
+        item2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemPhysFragment dialog = ItemPhysFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemPhysFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("anhur_item2_name", name);
+                        saveImage("anhur_item2", resourceId);
+                        item2.setImageResource(resourceId);
+                        item2Name.setText(name);
+                    }
+                });
+                dialog.show(getFragmentManager(), "item2Dialog");
+            }
+        });
+
+        item3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemPhysFragment dialog = ItemPhysFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemPhysFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("anhur_item3_name", name);
+                        saveImage("anhur_item3", resourceId);
+                        item3.setImageResource(resourceId);
+                        item3Name.setText(name);
+                    }
+                });
+                dialog.show(getFragmentManager(), "item3Dialog");
+            }
+        });
+
+        item4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemPhysFragment dialog = ItemPhysFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemPhysFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("anhur_item4_name", name);
+                        saveImage("anhur_item4", resourceId);
+                        item4.setImageResource(resourceId);
+                        item4Name.setText(name);
+                    }
+                });
+                dialog.show(getFragmentManager(), "item4Dialog");
+            }
+        });
+
+        item5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemPhysFragment dialog = ItemPhysFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemPhysFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("anhur_item5_name", name);
+                        saveImage("anhur_item5", resourceId);
+                        item5.setImageResource(resourceId);
+                        item5Name.setText(name);
+                    }
+                });
+                dialog.show(getFragmentManager(), "item5Dialog");
+            }
+        });
+
+        item6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ItemPhysFragment dialog = ItemPhysFragment.newInstance();
+                dialog.setOnListItemSelectedListener(new ItemPhysFragment.OnListItemClickedListener() {
+                    public void onListItemClick(String name, int resourceId) {
+                        saveName("anhur_item6_name", name);
+                        saveImage("anhur_item6", resourceId);
+                        item6.setImageResource(resourceId);
+                        item6Name.setText(name);
+                    }
+                });
+                dialog.show(getFragmentManager(), "item6Dialog");
+            }
+        });
+
+        return myView;
+    }
+
+    public int getImage(String item){
+        SharedPreferences pref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        int id = pref.getInt(item, R.drawable.no_item);
+        return id;
+    }
+
+    public void saveImage(String item, int resourceId){
+        SharedPreferences starterSP = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = starterSP.edit();
+        edit.putInt(item, resourceId);
+        edit.apply();
+    }
+
+    public String getName(String item){
+        SharedPreferences pref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        String name = pref.getString(item, "");
+        return name;
+    }
+
+    public void saveName(String item, String name){
+        SharedPreferences starterSP = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = starterSP.edit();
+        edit.putString(item, name);
+        edit.apply();
+    }
+
+}
